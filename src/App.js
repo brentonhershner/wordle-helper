a845e2cba393629d731a7929693a398b12d50eb4
Brenton Hershner <brenthershner@gmail.com>
2022-02-27 00:24:45 -0800

    even better styling to match actual wordle

--- src/App.js
+++ src/App.js
@@ -1,46 +1,53 @@
 import React, { useEffect, useState } from "react";
-import "../styles/WordleHelper.css";
-import words from "an-array-of-english-words";
-
-const words5letters = words.filter((word) => word.length === 5);
+import "./App.css";
+import { words } from "./words.js";
+import unique from "just-unique";
 
 const WordleHelper = () => {
   const WORD_DISPLAY_LIMIT = 500;
 
-  const [possible, setPossible] = useState(words5letters);
+  const [possible, setPossible] = useState(words);
   const [excluded, setExcluded] = useState("");
-  const [located, setLocated] = useState([...Array(5).fill("")]);
+  const [misplaced, setMisplaced] = useState("");
+  const [located, setLocated] = useState(Array(5).fill(""));
 
   const changeLetter = (e, position) => {
     const form = e.target.form;
     const key = e.keyCode;
     let newLetter = "";
     if (key >= 65 && key <= 90) {
+      // a-z keys
       newLetter = e.key;
       if (position < form.elements.length - 1) {
         form.elements[position + 1].focus();
       }
-    } else if (key === 8 || key === 46) {
-      newLetter = "";
     } else if (key === 9) {
+      // tab
       return;
+    } else if (key === 46) {
+      // forward delete
+      newLetter = "";
+    } else if (key === 8) {
+      // backspace
+      newLetter = "";
+      if (position > 0) {
+        form.elements[position - 1].focus();
+      }
     } else {
       return;
     }
     const newLocated = located.slice();
-    newLocated.splice(position, 1, newLetter.toLowerCase());
+    newLocated.splice(position, 1, newLetter.toUpperCase());
     setLocated(newLocated);
     e.preventDefault();
   };
 
   const updateExcluded = (e) => {
-    setExcluded(e.target.value.toLowerCase());
+    setExcluded(unique(e.target.value.toUpperCase().split("")).join(""));
   };
 
-  const filterExcluded = (word) => {
-    return !excluded
-      .split("")
-      .some((excludedLetter) => word.includes(excludedLetter));
+  const updateMisplaced = (e) => {
+    setMisplaced(e.target.value.toUpperCase());
   };
 
   const filterLocated = (word) => {
@@ -49,36 +56,47 @@ const WordleHelper = () => {
     });
   };
 
+  const filterExcluded = (word) => {
+    return !excluded
+      .split("")
+      .some((excludedLetter) => word.includes(excludedLetter));
+  };
+
+  const filterMisplaced = (candidateWord) => {
+    return (
+      true !==
+      misplaced.split("").reduce((word, letter) => {
+        if (word === true) {
+          return true;
+        }
+        const index = word.indexOf(letter);
+        if (index === -1) {
+          return true;
+        }
+        return word.slice(0, index).concat(word.slice(index + 1));
+      }, candidateWord.split(""))
+    );
+  };
+
   useEffect(() => {
     if (!excluded) {
-      setPossible(words5letters);
+      setPossible(words);
     }
-    const updatedList = words5letters
+    const updatedList = words
       .filter(filterExcluded)
+      .filter(filterMisplaced)
       .filter(filterLocated);
 
     setPossible(updatedList);
-  }, [excluded, located]);
-
-  useEffect(() => {
-    console.log(`Possibilities: ${possible.length}`);
-  }, [possible]);
+  }, [excluded, located, misplaced]);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Wordle Helper</h1>
 
-        <h2 className="label">Not Used Letters</h2>
-        <input
-          name="notUsed"
-          type={"text"}
-          value={excluded}
-          onChange={updateExcluded}
-        />
-
-        <h2 className="label">Located Letters</h2>
-        <form className="">
+        <h2 className="label">Correct Letters</h2>
+        <form className="inputs">
           {located.map((value, position) => {
             return (
               <input
@@ -91,11 +109,29 @@ const WordleHelper = () => {
               />
             );
           })}
+
+          <h2 className="label">Misplaced but Correct</h2>
+          <input
+            className="misplaced"
+            name="misplaced"
+            type={"text"}
+            value={misplaced}
+            onChange={updateMisplaced}
+          />
+
+          <h2 className="label">Rejects</h2>
+          <input
+            className="rejects"
+            name="rejects"
+            type={"text"}
+            value={excluded}
+            onChange={updateExcluded}
+          />
         </form>
       </header>
       <div className="results">
         <h2 className="label">{possible.length} possible words</h2>
-        {excluded || !located.every((l) => l === "") ? (
+        {excluded || misplaced || !located.every((l) => !l) ? (
           <>
             <ul className="list">
               {possible.slice(0, WORD_DISPLAY_LIMIT).map((word) => (
